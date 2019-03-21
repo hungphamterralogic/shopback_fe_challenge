f58167cc86995bd4f37f2dc20c25e154354af91a
Hung Pham <hung.pham@terralogic.com>
2019-03-21 15:58:30 +0700

    test: storeItem component

---
+++ src/components/storeItem/__tests__/storeItem.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactTestRenderer from 'react-test-renderer';
+import StoreItem from '../storeItem';
+import detectDevice from '../../../utils/detectDevice';
+
+const id = 'idTest';
+const item = {
+  id,
+  logoUrl: 'logoUrlTest',
+  displayText: 'displayTextTest'
+};
+
+describe('StoreItem', () => {
+  describe('display properly', () => {
+    test('is mobile', () => {
+      jest.spyOn(detectDevice, 'isMobile').mockImplementationOnce(() => true);
+
+      const selectStore = () => {};
+      const renderer = ReactTestRenderer.create(
+        <StoreItem item={item} selectStore={selectStore} />
+      );
+
+      expect(renderer.toJSON()).toMatchSnapshot();
+    });
+
+    test('is not mobile', () => {
+      jest.spyOn(detectDevice, 'isMobile').mockImplementationOnce(() => false);
+
+      const selectStore = () => {};
+      const renderer = ReactTestRenderer.create(
+        <StoreItem item={item} selectStore={selectStore} />
+      );
+
+      expect(renderer.toJSON()).toMatchSnapshot();
+    });
+  });
+
+  test('on click', () => {
+    const selectStore = jest.fn();
+    const renderer = ReactTestRenderer.create(<StoreItem item={item} selectStore={selectStore} />);
+
+    (function clickOnStoreItem() {
+      const storeItem = renderer.root.find(el => el.type === 'div');
+      storeItem.props.onClick();
+    })();
+
+    expect(selectStore.mock.calls.length).toBe(1);
+    expect(selectStore).toHaveBeenCalledWith(id);
+  });
+});
---
+++ src/components/storeItem/__tests__/storeItemContainer.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { genInitialMockStore, renderWithStore } from '../../../test/reduxComponentTestHelpers';
+
+import StoreItemContainer from '../storeItemContainer';
+
+import * as storesAction from '../../../actions/storesAction';
+import localStorage from '../../../utils/localStorage';
+
+describe('StoreItemContainer', () => {
+  const item = {
+    id: 'idTest',
+    logoUrl: 'logoUrlTest',
+    displayText: 'displayTextTest'
+  };
+
+  test('display properly', () => {
+    const store = genInitialMockStore();
+    const renderer = renderWithStore(<StoreItemContainer item={item} />, store);
+
+    expect(renderer.toJSON()).toMatchSnapshot();
+  });
+
+  test('on click', () => {
+    const selectStoreMock = jest.spyOn(storesAction, 'selectStore');
+    const setValueIntoKeyMock = jest.spyOn(localStorage, 'setValueIntoKey');
+
+    const store = genInitialMockStore();
+    const renderer = renderWithStore(<StoreItemContainer item={item} />, store);
+
+    (function clickOnStoreItem() {
+      const storeItem = renderer.root.find(el => el.type === 'div');
+      storeItem.props.onClick();
+    })();
+
+    expect(selectStoreMock.mock.calls.length).toBe(1);
+    expect(selectStoreMock).toBeCalledWith(item.id);
+
+    expect(setValueIntoKeyMock.mock.calls.length).toBe(1);
+    expect(setValueIntoKeyMock).toBeCalledWith('store', item.id);
+  });
+});
