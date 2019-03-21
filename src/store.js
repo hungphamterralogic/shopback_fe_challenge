310e82bceddb06a6fcb3db889c6c026fda6c7729
Hung Pham <hung.pham@terralogic.com>
2019-03-21 15:58:17 +0700

    chore: enable redux dev tool on browser

--- src/store.js
+++ src/store.js
@@ -1,6 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers/rootReducer';
 
-export default function configureStore(initialState = {}) {
-  return createStore(rootReducer, initialState, applyMiddleware());
+// eslint-disable-next-line no-underscore-dangle
+const devToolsCompose = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = devToolsCompose || compose;
+
+export default function configureStore(initialState = {}, extraMiddlewares = []) {
+  const enhancer = composeEnhancers(applyMiddleware(...extraMiddlewares));
+  return createStore(rootReducer, initialState, enhancer);
 }
