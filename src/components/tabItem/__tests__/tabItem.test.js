67e5b75cc469813cf9887b831573b33e18dc1b77
Hung Pham <hung.pham@terralogic.com>
2019-03-22 10:47:21 +0700

    test: tabItem component

---
+++ src/components/tabItem/__tests__/tabItem.test.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import ReactTestRenderer from 'react-test-renderer';
+
+import TabItem from '../tabItem';
+
+import detectDevice from '../../../utils/detectDevice';
+
+describe('TabItem', () => {
+  const props = {
+    selectedCategoryId: 'selectedCategoryId',
+    id: 'idTest',
+    name: 'nameTest',
+    iconUrl: 'iconUrlTest',
+    selectCategory: () => {}
+  };
+
+  describe('display properly', () => {
+    describe('is selected', () => {
+      beforeAll(() => {
+        props.selectedCategoryId = props.id;
+      });
+
+      test('is mobile', () => {
+        jest.spyOn(detectDevice, 'isMobile').mockImplementationOnce(() => true);
+
+        const renderer = ReactTestRenderer.create(<TabItem {...props} />);
+        expect(renderer.toJSON()).toMatchSnapshot();
+      });
+
+      test('is not mobile', () => {
+        jest.spyOn(detectDevice, 'isMobile').mockImplementationOnce(() => false);
+
+        const renderer = ReactTestRenderer.create(<TabItem {...props} />);
+        expect(renderer.toJSON()).toMatchSnapshot();
+      });
+    });
+
+    describe('is not selected', () => {
+      beforeAll(() => {
+        props.selectedCategoryId = `differentThan${props.id}`;
+      });
+
+      test('is mobile', () => {
+        jest.spyOn(detectDevice, 'isMobile').mockImplementationOnce(() => true);
+
+        const renderer = ReactTestRenderer.create(<TabItem {...props} />);
+        expect(renderer.toJSON()).toMatchSnapshot();
+      });
+
+      test('is not mobile', () => {
+        jest.spyOn(detectDevice, 'isMobile').mockImplementationOnce(() => false);
+
+        const renderer = ReactTestRenderer.create(<TabItem {...props} />);
+        expect(renderer.toJSON()).toMatchSnapshot();
+      });
+    });
+  });
+
+  test('on click', () => {
+    props.selectCategory = jest.fn();
+
+    const renderer = ReactTestRenderer.create(<TabItem {...props} />);
+
+    (function clickOnTabItem() {
+      const tabItem = renderer.root.find(el => el.type === 'div');
+      tabItem.props.onClick();
+    })();
+
+    expect(props.selectCategory.mock.calls.length).toBe(1);
+    expect(props.selectCategory).toHaveBeenCalledWith(props.id);
+  });
+});
---
+++ src/components/tabItem/__tests__/tabItemContainer.test.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { genInitialMockStore, renderWithStore } from '../../../test/reduxComponentTestHelpers';
+
+import TabItemContainer from '../tabItemContainer';
+
+import detectDevice from '../../../utils/detectDevice';
+
+import * as categoriesAction from '../../../actions/categoriesAction';
+import localStorage from '../../../utils/localStorage';
+
+describe('TabItemContainer', () => {
+  const props = {
+    id: 'idTest',
+    name: 'nameTest',
+    iconUrl: 'iconUrlTest'
+  };
+
+  describe('display properly', () => {
+    describe('is selected', () => {
+      let mockStore;
+
+      beforeAll(() => {
+        mockStore = genInitialMockStore({
+          'categories.selected': props.id
+        });
+      });
+
+      test('is mobile', () => {
+        jest.spyOn(detectDevice, 'isMobile').mockImplementationOnce(() => true);
+
+        const renderer = renderWithStore(<TabItemContainer {...props} />, mockStore);
+        expect(renderer.toJSON()).toMatchSnapshot();
+      });
+
+      test('is not mobile', () => {
+        jest.spyOn(detectDevice, 'isMobile').mockImplementationOnce(() => false);
+
+        const renderer = renderWithStore(<TabItemContainer {...props} />, mockStore);
+        expect(renderer.toJSON()).toMatchSnapshot();
+      });
+    });
+
+    describe('is not selected', () => {
+      let mockStore;
+
+      beforeAll(() => {
+        mockStore = genInitialMockStore({
+          'categories.selected': `differentThan${props.id}`
+        });
+      });
+
+      test('is mobile', () => {
+        jest.spyOn(detectDevice, 'isMobile').mockImplementationOnce(() => true);
+
+        const renderer = renderWithStore(<TabItemContainer {...props} />, mockStore);
+        expect(renderer.toJSON()).toMatchSnapshot();
+      });
+
+      test('is not mobile', () => {
+        jest.spyOn(detectDevice, 'isMobile').mockImplementationOnce(() => false);
+
+        const renderer = renderWithStore(<TabItemContainer {...props} />, mockStore);
+        expect(renderer.toJSON()).toMatchSnapshot();
+      });
+    });
+  });
+
+  test('on click', () => {
+    const selectCategoryMock = jest.spyOn(categoriesAction, 'selectCategory');
+    const setValueIntoKeyMock = jest.spyOn(localStorage, 'setValueIntoKey');
+
+    const mockStore = genInitialMockStore();
+    const renderer = renderWithStore(<TabItemContainer {...props} />, mockStore);
+
+    (function clickOnTabItem() {
+      const tabItem = renderer.root.find(el => el.type === 'div');
+      tabItem.props.onClick();
+    })();
+
+    expect(selectCategoryMock.mock.calls.length).toBe(1);
+    expect(selectCategoryMock).toBeCalledWith(props.id);
+
+    expect(setValueIntoKeyMock.mock.calls.length).toBe(1);
+    expect(setValueIntoKeyMock).toBeCalledWith('category', props.id);
+  });
+});
